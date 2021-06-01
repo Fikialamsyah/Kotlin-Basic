41debb757e087d330e341b5f50adb836ce8f1de9
fiki <fiki@pop-os.localdomain>
2021-06-01 23:59:33 +0700

    add escaped string and raw string

--- src/fundamental/string.kt
+++ src/fundamental/string.kt
@@ -7,4 +7,25 @@ fun main() {
 
     // cetak index 0 dari text
     println("First character of $text is $firstChar") // $ menggunakan string interpolation
+
+    // looping string
+    for (char in text){
+        print("$char ")
+    }
+
+    // escaped string
+    val statement = "Kotlin is \"Awesome\""
+    println(statement)
+
+    // menambahkan unicode
+    val name = "Unicode test : \u00A9"
+    println(name)
+
+    // raw string
+    val line ="""
+        Line 1
+        Line 2
+        Line 3
+    """.trimIndent()
+    println(line)
 }
