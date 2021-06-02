5cc290fa3153c101d0bc3265f20b012931f21439
fiki <fiki@pop-os.localdomain>
2021-06-02 08:01:18 +0700

    solved typo comment

--- src/collection/sequence.kt
+++ src/collection/sequence.kt
@@ -5,7 +5,7 @@ fun main() {
     // membuat sequence
     list.filter { it % 5 == 0 }.map { it * 2 }.forEach { println(it) }
 
-    // mengugunakan generateSequence
+    // mengunakan generateSequence
     val sequenceNumber = generateSequence(1) { it + 1 }
     sequenceNumber.take(5).forEach { print(it) }
 }
